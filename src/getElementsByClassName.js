0208f19a94858c1ba9127356bf81ddc8d09100b4
SaintEmbers <quantum108@gmail.com>
2015-02-22 17:21:18 -0600

    getElementsByClassName from scratch

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  	var elementals = [];
+  	function goThruDOM(node, f){
+  		f(node);
+  		var i = 0;
+  		node = node.childNodes[i];
+  		i++;
+  		while(node != null){
+  			goThruDOM(node,f);
+  			node = node.nextSibling
+  		}
+  	}
+  	
+  	goThruDOM(document.body, function(node){
+  		var classy = node.classList;
+  		if(classy){
+  		 	for(var i = 0; i < classy.length; i++){
+  		 		if(classy[i] === className)
+  		 			elementals.push(node);
+  		 	}
+  		}
+  	})
+  	return elementals;
 };
